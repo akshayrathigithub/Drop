97239c2c8426c04d605fa320ed85b26ce91a8ec8
Akshay Rathi <akshayrathi96@gmail.com>
2022-05-01 23:45:55 +0530

    some more changes

--- src/Interface/Interface.ts
+++ src/Interface/Interface.ts
@@ -1,7 +1,13 @@
+import { ReactNode } from 'react';
+
 export interface RefType {
   element: HTMLSpanElement | HTMLDivElement | null;
 }
 
 export interface LoaderProps {
   hideLoader: () => void;
+}
+
+export interface ReactComponent {
+  children?: ReactNode;
 }
