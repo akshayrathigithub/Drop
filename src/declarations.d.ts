9f8532563a613318932ced8b59f745f31e0dd9a0
Akshay Rathi <akshayrathi96@gmail.com>
2022-05-01 15:37:26 +0530

    formatting changes

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,20 +1,20 @@
-declare module '*.png'
+declare module '*.png';
 declare module '*.svg' {
-  import * as React from 'react'
+  import * as React from 'react';
 
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
-  >
+  >;
 
-  const SRC: string
-  export default SRC
+  const SRC: string;
+  export default SRC;
 }
 
-declare module '*.jpg'
-declare module '*.pdf'
+declare module '*.jpg';
+declare module '*.pdf';
 
 declare global {
   interface Window {
-    location: Location
+    location: Location;
   }
 }
