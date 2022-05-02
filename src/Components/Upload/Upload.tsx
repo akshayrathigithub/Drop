213f1e04e0e173f2d97a112a23afd6b3ccf34e6e
Akshay Rathi <akshayrathi96@gmail.com>
2022-05-02 23:11:34 +0530

    upload component updated

--- src/Components/Upload/Upload.tsx
+++ src/Components/Upload/Upload.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import AnimatingIcon from '../../SharedComponents/AnimatingIcon/AnimatingIcon';
+import AnimatingIcon from '../../SharedComponents/AnimatingComponent/AnimatingComponent';
 import Container from '../../SharedComponents/Container/Container';
+import DragNDrop from '../DragNDrop/DragNDrop';
 import { UploadProps } from './Upload.interface';
 import './Upload.scss';
 
@@ -9,7 +10,25 @@ const Upload: React.FC<UploadProps> = (props) => {
     <Container>
       <div className="upload-wrapper">
         <div className="heading">Secure Cloud Storage And Communication</div>
-        <AnimatingIcon />
+        <div className="animating-one">
+          <AnimatingIcon text="hello" />
+        </div>
+
+        <div className="animating-two">
+          <AnimatingIcon color="red" />
+        </div>
+
+        <div className="animating-three">
+          <AnimatingIcon text="new" />
+        </div>
+
+        <div className="animating-four">
+          <AnimatingIcon color="blue" />
+        </div>
+
+        <div className="drag-N-drop">
+          <DragNDrop />
+        </div>
       </div>
     </Container>
   );
