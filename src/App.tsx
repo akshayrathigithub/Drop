734762a1c49d964cfc397b40d9ef2aae8e786b9a
Akshay Rathi <akshayrathi96@gmail.com>
2022-05-06 22:24:37 +0530

    upload component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Upload from './Components/Upload/Upload';
 import ThemeProvider from './Contexts/ThemeProvider';
 import './GlobalStyle.scss';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
-      <div className="main-app">This is the App Component !!!</div>
+      <div className="main-app">
+        <Upload />
+      </div>
     </ThemeProvider>
   );
 };
